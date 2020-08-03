1caf34b2fff730a9c657c7bc7422f92793e7f4d1
Brian Wallace <brian.wallace@mottmac.com>
2020-08-03 15:07:28 -0500

    save

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -5,7 +5,7 @@ $console.WindowTitle = "Brian's PowerShell"
 $console.BackgroundColor = "Black"
 $console.ForegroundColor = "White"
 Set-ItemProperty -Path HKCU:\Console -Name WindowsAlpha -Value 205
-Import-Module MyPowerShellModule
+Import-Module "C:\Users\wal63291\Documents\WindowsPowerShell\Powershell\Modules\MyPowershellModule\MyPowershellModule.psm1"
 Import-Module ActiveDirectory
 Clear-Host
 Write-Host "Welcome back Brian"
