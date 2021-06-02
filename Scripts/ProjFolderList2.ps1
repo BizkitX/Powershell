cbbe41f8dcca3661e584bbc00f7c88fac5b2f541
Brian Wallace <brian.m.wallace@outlook.com>
2021-06-02 16:54:17 -0500

    update

--- Scripts/ProjFolderList2.ps1
+++ Scripts/ProjFolderList2.ps1
@@ -3,4 +3,4 @@ Select-Object FullName |
 ForEach-Object -Process{New-Object -TypeName PSObject -Property @{Name =$_.FullName;Size = (Get-ChildItem -path $_.FullName -Recurse -Force -ErrorAction SilentlyContinue | 
 Measure-Object -Property Length -Sum ).Sum/1gb}} | 
 Select-Object Name, @{Name="Size(GB)";Expression={("{0:N2}" -f($_.Size))}} |
-Out-GridView
+Export-Csv "insert path"
