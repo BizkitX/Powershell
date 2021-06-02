20c4658aa64203c7ae053d7cd3d6f174c372d4df
Brian Wallace <brian.m.wallace@outlook.com>
2021-06-02 17:19:48 -0500

    y

--- Scripts/ProjFolderList2.ps1
+++ Scripts/ProjFolderList2.ps1
@@ -1,4 +1,4 @@
-Get-ChildItem -path "C:\users\wal63291\Desktop" -Directory -Recurse -Depth 2 | 
+Get-ChildItem -path "C:\users\wal63291\Desktop" -Directory -Recurse -Depth 1 | 
 Select-Object FullName | 
 ForEach-Object -Process{New-Object -TypeName PSObject -Property @{Name =$_.FullName;Size = (Get-ChildItem -path $_.FullName -Recurse -Force -ErrorAction SilentlyContinue | 
 Measure-Object -Property Length -Sum ).Sum/1gb}} | 
