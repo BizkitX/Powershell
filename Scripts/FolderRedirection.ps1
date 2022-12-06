e9f125a010c804bac3485458fc20072ca3953f85
Brian Wallace <brian.m.wallace@outlook.com>
2022-12-06 10:18:05 -0600

    notes added

--- Scripts/FolderRedirection.ps1
+++ Scripts/FolderRedirection.ps1
@@ -1,3 +1,5 @@
+#Finds all user accounts that are not a member of the 'Folder Redirection Users' group in AD and exports the list to a CSV file.
+
 Get-ADUser -Filter * -Properties Name, MemberOf, Description |
 Where-Object {$_.MemberOf -notcontains "CN=Folder Redirection Users,CN=Users,DC=ad,DC=dixieelectric,DC=coop" -and $_.Enabled -eq $True} |
 Select-Object Name, Enabled, Description, MemberOf |
