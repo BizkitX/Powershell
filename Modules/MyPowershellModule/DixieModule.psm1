e482b604b85dc9abe1f2f7025d15921045ec4b66
Brian Wallace <brian.m.wallace@outlook.com>
2022-12-12 09:49:44 -0600

    update

--- Modules/MyPowershellModule/DixieModule.psm1
+++ Modules/MyPowershellModule/DixieModule.psm1
@@ -61,3 +61,23 @@ Write-Host -ForegroundColor Red "The computer is unreachable"
     
 }
 
+# Will remove the win32 user profile from the specified computer. 
+# Be sure to change computername and username.
+
+function RemoveWindowsProfile {
+    param(
+      [parameter(Mandatory)] 
+        [string[]] 
+        $ComputerName,
+    
+      [Parameter(Mandatory)]
+        [string[]]
+        $UserName
+    
+    )
+    Get-CimInstance -ComputerName $ComputerName -Class Win32_UserProfile |
+    Where-Object { $_.LocalPath.split('\')[-1] -eq $UserName } |
+    Remove-CimInstance -Verbose
+    
+    }
+
